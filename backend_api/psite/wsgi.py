c68ccecd54016565ad626b8c06f94a49e605a460
ogsahil <contactsahilpnayak@gmail.com>
2024-07-27 17:31:24 +0530

    deleting the existing vercel configs

--- backend_api/psite/wsgi.py
+++ backend_api/psite/wsgi.py
@@ -14,7 +14,3 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'psite.settings')
 
 application = get_wsgi_application()
-
-#changes for deployment :
-
-app = application
