f47e5eb65737f982d191bd7098e5171d8599f9a5
ogsahil <contactsahilpnayak@gmail.com>
2024-07-27 16:09:02 +0530

    changes in vercel.json - backend_api

--- backend_api/psite/wsgi.py
+++ backend_api/psite/wsgi.py
@@ -14,3 +14,7 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'psite.settings')
 
 application = get_wsgi_application()
+
+#changes for deployment :
+
+app = application
