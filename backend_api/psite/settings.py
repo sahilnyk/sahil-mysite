267ea474d776d9f65f8a9323d0e0d9e36821db73
ogsahil <contactsahilpnayak@gmail.com>
2024-07-28 00:21:58 +0530

    Deploying in render for backend

--- backend_api/psite/settings.py
+++ backend_api/psite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'default-secret-key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost','127.0.0.1','.vercel.app']
+ALLOWED_HOSTS = ['localhost','127.0.0.1','.vercel.app','.onrender.com']
 
 # Application definition
 
