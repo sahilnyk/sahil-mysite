484d6d4bc0ef5628e531a0f3e86359760261b6e0
ogsahil <contactsahilpnayak@gmail.com>
2024-07-28 12:23:14 +0530

    Added the build file and vercel.json

--- backend_api/psite/settings.py
+++ backend_api/psite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'default-secret-key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost','127.0.0.1','.vercel.app','.onrender.com']
+ALLOWED_HOSTS = ['localhost','127.0.0.1','.vercel.app','.onrender.com','.now.sh']
 
 # Application definition
 
@@ -148,9 +148,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+# STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+STATICFILES_DIRS = os.path.join(BASE_DIR,'static'),
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles_build','static')
