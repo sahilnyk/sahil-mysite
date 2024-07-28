475346e748894a4a7ed0035b27766f363c800a3f
ogsahil <contactsahilpnayak@gmail.com>
2024-07-28 12:32:16 +0530

    Deleted

--- backend_api/build_files.sh
+++
@@ -1,8 +0,0 @@
-echo "BUILD START"
-
-# Use the specific Python version
-python3.10 -m pip install --upgrade pip
-python3.10 -m pip install -r requirements.txt
-python3.10 manage.py collectstatic --noinput --clear
-
-echo "BUILD END"
